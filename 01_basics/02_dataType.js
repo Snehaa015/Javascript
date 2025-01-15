70f394771d26b32f4f414234a2be8872109377a4
Snehaa015 <143495940+Snehaa015@users.noreply.github.com>
2025-01-15 12:54:51 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,35 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3
+    +3)// code redability should be high
+    //ecmascript javascript standards site ie. tc39 and mdn.
+    
+
+
+
+console.log("sneha")
+
+let name = "sneha"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//primitive datatypes=>
+//number=> 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null =>standalone value
+//undefined => 
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof null);//output=undefined
+console.log(typeof undefined);//output=object
+
+
