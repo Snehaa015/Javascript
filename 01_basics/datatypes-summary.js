94f5024064f5dcc3ac6b8d61bd3df8748b380af9
Snehaa015 <143495940+Snehaa015@users.noreply.github.com>
2025-01-16 15:01:40 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// // primitive
+
+// //7 types : string,number,boolean,null,undefined,symbol,Bigint
+
+// // const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = symbol('123')
+
+// //console.log(id ===anotherId);
+
+// const bigNumber = 345467548497496n
+
+
+// //reference (Non primitive)
+
+// //array,objects,functions
+
+// const heros = ["shaktiman","naagraj","doga"]
+// let myObj = {
+//     name:"sneha";
+//     Age:20;
+// }
+
+// const myFunction =function(){
+//     console.log("hello world");
+// }
+
+// console.log(typeof scoreValue);
