2507e96958693232d851555c66cc07b81461aff0
Snehaa015 <143495940+Snehaa015@users.noreply.github.com>
2025-01-18 13:57:11 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "sneha"
+const repoCount =5
+
+//console.log(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} and my repo count is ${5}`);
+
+const gameName = new String('sneha-c')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt('2'));
+console.log(gameName.indexOf('e'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "  sneha  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
