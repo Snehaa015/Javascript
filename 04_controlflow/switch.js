90f5d67fe12297a260845fe8ecefc7d8191651b2
Snehaa015 <143495940+Snehaa015@users.noreply.github.com>
2025-01-28 17:20:41 +0000

    controlflow

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// } basic syntax
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        
+        break;//to duplicate this shift+alt+downarray
+    case "feb":
+        console.log("feb");
+        
+        break;
+     case "march":
+        console.log("march");
+        
+        break;
+    case "april":
+        console.log("april");
+        
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
